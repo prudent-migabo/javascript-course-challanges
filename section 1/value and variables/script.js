c838a8e32ec054a728fcf575ead2b9c1e6373459
prudent16 <lyadungamigaboprudent@gmail.com>
2023-04-12 14:00:17 +0200

    feat: challange 1 values and variables

---
+++ section 1/value and variables/script.js
@@ -0,0 +1,7 @@
+let country = 'democratic republic of the congo';
+let continent = 'africa';
+let population = '2000000000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
