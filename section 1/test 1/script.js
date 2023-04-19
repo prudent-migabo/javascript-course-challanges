5d0be9205d99f831f7cab30c077d1d2de37e6538
prudent16 <lyadungamigaboprudent@gmail.com>
2023-04-19 20:49:24 +0200

    feat: test challange 1

---
+++ section 1/test 1/script.js
@@ -0,0 +1,30 @@
+// Data1
+let weightMark = 78;
+let heightMark = 1.69;
+
+let weightJohn = 92;
+let heightJohn = 1.96;
+
+let markBMI = weightMark / heightMark ** 2; // 27.30
+let johnBMI = weightJohn / heightJohn ** 2; // 23.94
+
+console.log(markBMI, johnBMI);
+
+let markHeigherBMI = markBMI > johnBMI;
+console.log(markHeigherBMI);
+
+
+// Data 2
+weightMark = 95;
+heightMark = 1.88;
+
+weightJohn = 85;
+heightJohn = 1.76;
+
+markBMI = weightMark / heightMark ** 2; // 26.87
+johnBMI = weightJohn / heightJohn ** 2; // 27.44
+
+console.log(markBMI, johnBMI);
+
+markHeigherBMI = markBMI > johnBMI;
+console.log(markHeigherBMI);
