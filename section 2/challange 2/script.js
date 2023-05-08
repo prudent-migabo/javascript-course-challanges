85e49885a9baf7b79a2f0a448afb01e9bee8f8fa
prudent16 <lyadungamigaboprudent@gmail.com>
2023-05-08 21:53:56 +0300

    feat: section2, challenge2

---
+++ section 2/challange 2/script.js
@@ -0,0 +1,16 @@
+function calcTip(billValue) {
+    if (billValue >= 50 && billValue <= 300) {
+        return (billValue * 15) / 100;
+    } else {
+        return (billValue * 20) / 100;
+    }
+}
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills);
+console.log(tips, total);
